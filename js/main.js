a7cf6cb4068a2a313525f967a66a2d31c417f824
Fabio <thebloodshadow14@gmail.com>
2023-10-18 16:21:02 +0200

    INIT - setup

---
+++ js/main.js
@@ -0,0 +1,10 @@
+"use strict;";
+
+const rawTeamMembers = [
+  { fullName: "Wayne Barnett", role: "Founder & CEO", photoSrc: "../img/wayne-barnett-founder-ceo.jpg" },
+  { fullName: "Angela Caroll", role: "Chief Editor", photoSrc: "../img/angela-caroll-chief-editor.jpg" },
+  { fullName: "Walter Gordon", role: "Office Manager", photoSrc: "../img/walter-gordon-office-manager.jpg" },
+  { fullName: "Angela Lopez", role: "Social Media Manager", photoSrc: "../img/angela-lopez-social-media-manager.jpg" },
+  { fullName: "Scott Estrada", role: "Developer", photoSrc: "../img/scott-estrada-developer.jpg" },
+  { fullName: "Barbara Ramos", role: "Graphic Designer", photoSrc: "../img/barbara-ramos-graphic-designer.jpg" },
+];
