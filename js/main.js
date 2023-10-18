f44a47f3c49f6e424375df166d10c7ad6e6d73fe
Fabio <thebloodshadow14@gmail.com>
2023-10-18 16:52:38 +0200

    ADD - rawTeamMembers | js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 "use strict;";
 
+const domGrid = document.querySelector(".grid");
+
 const rawTeamMembers = [
   { fullName: "Wayne Barnett", role: "Founder & CEO", photoSrc: "../img/wayne-barnett-founder-ceo.jpg" },
   { fullName: "Angela Caroll", role: "Chief Editor", photoSrc: "../img/angela-caroll-chief-editor.jpg" },
@@ -8,3 +10,16 @@ const rawTeamMembers = [
   { fullName: "Scott Estrada", role: "Developer", photoSrc: "../img/scott-estrada-developer.jpg" },
   { fullName: "Barbara Ramos", role: "Graphic Designer", photoSrc: "../img/barbara-ramos-graphic-designer.jpg" },
 ];
+
+for (const member of rawTeamMembers) {
+  const tempElement = document.createElement("div");
+  tempElement.classList.add("flex", "flex-col", "bg-white", "drop-shadow-md", "rounded-xl");
+  tempElement.innerHTML = `
+  <img src="${member.photoSrc}" class="rounded-t-xl" />
+  <div class="flex flex-col p-4 lg:p-6 items-center justify-center">
+    <h1 class="font-bold text-lg text-slate-800">${member.fullName}</h1>
+    <span class="text-md text-slate-600">${member.role}</span>
+  </div>
+  `;
+  domGrid.appendChild(tempElement);
+}
